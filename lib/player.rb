95780a83ba5cca1822fabc2b070c61e13d1878c7
Ben <ben.c.hawker@gmail.com>
2016-01-22 21:30:43 +0800

    Starts player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,33 @@
 class Player
 
+  def self.male
+    Male.new
+  end
+
+  def self.female
+    Female.new
+  end
+
+  class Male < Player
+    def male?
+      true
+    end
+
+  end
+
+  class Female < Player
+    def male?
+      false
+    end
+
+  end
+
+  def suspended?
+
+  end
+
+  def suspend(number_of_games)
+
+  end
 
 end
