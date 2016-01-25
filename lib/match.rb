52826d0e8323d06c82fda422d2f0c914e3eb7f6c
Ben <ben.c.hawker@gmail.com>
2016-01-25 22:25:15 +0800

    Further changes

--- lib/match.rb
+++ lib/match.rb
@@ -8,13 +8,22 @@ class Match
   end
 
   def match_conditions_are_met?
+    !any_players_suspended?
+    both_teams_valid?
+  end
 
+  def both_teams_valid?
+    @team_one.valid_team? && @team_two.valid_team?
   end
 
   private
 
-  def check_player_is_free_agent(team)
-    team.players.each
+  def any_players_suspended?
+    look_for_suspended_players(team_one) && look_for_suspended_players(team_two)
+  end
+
+  def look_for_suspended_players(team)
+    team.players.all? { |player| player.suspended == true }
   end
 
 end
--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -1,14 +1,40 @@
 require 'match'
+require 'team'
+require 'player'
 
 describe Match do
 
-	let(:match) { Match.new }
+  let(:male) { Player::Male.new }
+  let(:female) { Player::Female.new }
+  let(:team_one) { Team.new }
+  let(:team_two) { Team.new }
+	let(:match) { Match.new(team_one, team_two) }
 
-	describe "match_conditions_are_met?" do
+	describe "#match_conditions_are_met?" do
+		it "returns false if the match conditions are not met" do
+			expect(match.match_conditions_are_met?).to eq false
+		end
+
 		it "returns true if the match conditions are met" do
-			
+			expect(match.match_conditions_are_met?).to eq true
 		end
 	end
+
+	describe "#any_players_suspended?" do
+		it "returns true if any players are suspended in either team" do
+			player = Player.new
+			player.suspended = true
+			team_one.players << player
+			expect(match.send(:any_players_suspended?)).to be true
+		end
+
+		it "returns true if any players are suspended in either team" do
+			player = Player.new
+			team_one.players << player
+			expect(match.send(:any_players_suspended?)).to be false
+		end
+	end
+
 
 
 end
